28f8e046b38105874fd503402f755459557ac499
Alexey Ivannikov <alexey.a.ivannikov@gmail.com>
2024-05-21 22:15:23 +0300

    Added counter

--- src/components/dish/component.jsx
+++ src/components/dish/component.jsx
@@ -1,3 +1,19 @@
+import { useState } from "react";
+
 export const Dish = ({dish}) => {
-    return <span>{dish.name}</span>
+    const [count, setCount] = useState(0);
+
+    const decrement = () => {if (count > 0) setCount(count - 1); };
+    const increment = () => {if (count < 5) setCount(count + 1); };
+
+    return (
+        <div>
+            <span>{dish.name}</span>
+            <span>
+                <button onClick={decrement}>-</button>
+                {count}
+                <button onClick={increment}>+</button>
+            </span>
+        </div>
+    );
 };
