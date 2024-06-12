bd7b652c2a3cb68b130b0437afb38d979ddbbdfb
Alexey Ivannikov <alexey.a.ivannikov@gmail.com>
2024-06-12 16:18:19 +0300

    Incapsulate ScrollProgress

--- src/components/header/component.jsx
+++ src/components/header/component.jsx
@@ -1,13 +1,17 @@
 import { ThemeToggler } from "../../components/theme-toggler/component";
+import { ScrollProgress } from "../scroll-progres/component";
 import { UserLoginLogout } from "../user-login-logout/component";
 
 
 export const Header = () => {
     return (
         <div>
-            <span>Header</span>
-            <ThemeToggler />
-            <UserLoginLogout />
+            <ScrollProgress />
+            <div>
+                <span>Header</span>
+                <ThemeToggler />
+                <UserLoginLogout />
+            </div>
         </div>
     );
 };
