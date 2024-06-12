d6d378e0a25588bd0b8b146701705a370428dd7e
Alexey Ivannikov <alexey.a.ivannikov@gmail.com>
2024-06-12 16:18:48 +0300

    createPortal used

--- src/components/user-login-logout/component.jsx
+++ src/components/user-login-logout/component.jsx
@@ -1,21 +1,25 @@
-import { useContext } from "react";
+import { useState, useContext, useCallback } from "react";
 import { UserContext } from "../user-context/context";
 import { Button } from "../button/component";
-import { ModalContext } from "../modal-context/context";
+import { Modal } from "../modal/component";
+import { AuthForm } from "../auth-form/component";
 
 export const UserLoginLogout = () => {
     const { username, logoutUser } = useContext(UserContext);
 
-    const { setIsVisible } = useContext(ModalContext);
+    const [ isVisible, setIsVisible ] = useState(false);
+
+    const closeModal = useCallback(() => { setIsVisible(false) }, []);
 
     return (
         <div>
-            {username}
+            <span>{username}</span>
             {username ?
                 <Button onClick={logoutUser}>Logout</Button>
                 :
                 <Button onClick={() => setIsVisible(true)}>Login</Button>
             }
+            {isVisible && <Modal closeModal={closeModal} ><AuthForm closeForm={closeModal}></AuthForm></Modal>}
         </div>
     );
 };
