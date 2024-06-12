e102c84c867f4829e8e8ac6b078b958d8080559a
Alexey Ivannikov <alexey.a.ivannikov@gmail.com>
2024-06-12 16:29:03 +0300

    ScrollProgress moved to the Layout

--- src/components/header/component.jsx
+++ src/components/header/component.jsx
@@ -1,12 +1,10 @@
 import { ThemeToggler } from "../../components/theme-toggler/component";
-import { ScrollProgress } from "../scroll-progres/component";
 import { UserLoginLogout } from "../user-login-logout/component";
 
 
 export const Header = () => {
     return (
         <div>
-            <ScrollProgress />
             <div>
                 <span>Header</span>
                 <ThemeToggler />
--- src/components/layout/component.jsx
+++ src/components/layout/component.jsx
@@ -1,10 +1,12 @@
 import { Footer } from "../footer/component";
 import { Header } from "../header/component";
+import { ScrollProgress } from "../scroll-progres/component";
 
 export const Layout = ({ children }) => {
     return (
         <div>
-            <div id="modal" style={{ zIndex: 2, position: 'relative' }}></div>
+            <ScrollProgress/>
+            <div id="modal" style={{ zIndex: 2, position: 'relative'}}></div>
             <div style={{ zIndex: 1 }}>
                 <Header />
                 <div>{children}</div>
