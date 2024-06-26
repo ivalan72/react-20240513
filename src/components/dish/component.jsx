48b6bece416185893be9274d02570b61458beba4
Alexey Ivannikov <alexey.a.ivannikov@gmail.com>
2024-06-26 23:07:10 +0300

    Fix: returning the wrongly deleted code

--- src/components/dish/component.jsx
+++ src/components/dish/component.jsx
@@ -1,8 +1,11 @@
 import { useContext } from 'react';
+import { useCount} from '../../hooks/use-count';
 import { Counter } from '../counter/component';
 import { UserContext } from '../user-context/context';
 
 export const Dish = ({dish}) => {
+    const {count, decrement, increment} = useCount({initialValue: 0, min: 0, max: 5});
+
     const { username, authorizeUser, logoutUser} = useContext(UserContext);
 
     if (!dish) {
