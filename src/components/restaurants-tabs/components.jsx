3de95ce5bd69853e3818c99fd538cf8376288344
Alexey Ivannikov <alexey.a.ivannikov@gmail.com>
2024-06-30 23:20:31 +0300

    Filtration added

--- src/components/restaurants-tabs/components.jsx
+++ src/components/restaurants-tabs/components.jsx
@@ -1,15 +1,27 @@
+import { useSearchParams } from 'react-router-dom';
 import { RestaurantTab } from '../tab/container';
 
 export const RestaurantsTabs = ({restaurants}) => {
+    const [searchParams, setSearchParams] = useSearchParams();
+    const searchValue = searchParams.get('filter')?.toLowerCase() || '';
+
     if (!restaurants) {
         return null;
     }
 
     return (
         <div>
-            {restaurants.map((restaurant) => (
-                <RestaurantTab restaurant={restaurant} />
-            ))}
+            <span style={{marginRight: '1em'}}>
+                <input
+                    value={searchValue}
+                    onChange={(event) => setSearchParams({filter: event.target.value})} />
+            </span>
+            {restaurants
+                .filter(({name}) => name.toLowerCase().indexOf(searchValue) !== -1)
+                .map((restaurant) => (
+                    <RestaurantTab key={restaurant.id} restaurant={restaurant} />
+                ))
+            }
         </div>
     );
 };
